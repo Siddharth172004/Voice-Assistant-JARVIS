e56c3ed2cff000440024544fee495395622bb6a3
Siddharth Dhole <165372082+Siddharth172004@users.noreply.github.com>
2025-06-19 18:37:10 +0530

    Add files via upload

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,4 +1,4 @@
-music = {"knock" : "https://www.youtube.com/watch?v=Yo7q7rdRs7U&list=RDYo7q7rdRs7U&start_radio=1",
-         "joint" : "https://www.youtube.com/watch?v=ADVNZM-qrT4&list=RDADVNZM-qrT4&start_radio=1",
-          "sapphire" : "https://www.youtube.com/watch?v=JgDNFQ2RaLQ&list=RDJgDNFQ2RaLQ&start_radio=1",
-           "tere" : "https://youtu.be/uc43tD6-E4U?si=qGhRavPGERKeo7k4" }
+music = {"knock" : "https://www.youtube.com/watch?v=Yo7q7rdRs7U&list=RDYo7q7rdRs7U&start_radio=1", #knock knock
+         "joint" : "https://www.youtube.com/watch?v=ADVNZM-qrT4&list=RDADVNZM-qrT4&start_radio=1", #joint in a booth
+          "sapphire" : "https://www.youtube.com/watch?v=JgDNFQ2RaLQ&list=RDJgDNFQ2RaLQ&start_radio=1", #Sapphire 
+           "tere" : "https://youtu.be/uc43tD6-E4U?si=qGhRavPGERKeo7k4" } #tere naina
